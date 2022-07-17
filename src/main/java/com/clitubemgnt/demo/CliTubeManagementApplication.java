bff833b0c45a94d2f946f7d75042a8defa7e5cdc
Jacob Bayer <jbbuilder8@yahoo.com>
2022-07-17 14:24:18 -0400

    first commit

---
+++ src/main/java/com/clitubemgnt/demo/CliTubeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.clitubemgnt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliTubeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliTubeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clitubemgnt/demo/CliTubeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clitubemgnt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliTubeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
